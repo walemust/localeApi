ea68b0f398fb6322906d06d6ea613d6fe90a6ded
walemust <walemust28@gmail.com>
2023-07-04 20:03:13 +0100

    made correction to the db

--- utils/CONFIG/db.config.js
+++ utils/CONFIG/db.config.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 const CONFIG = require("./env.config")
+require("dotenv").config();
 
 // module.exports = connectDB;
-const connectDB = async()=>{
-try {
-    const conn = await  mongoose.connect(CONFIG.mongo_uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-     console.log(`MongoDB connected: ${conn.connection.host}`)
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log(`MongoDB connected: ${conn.connection.host}`)
 
-} catch (error) {
-    console.error(error);
-    process.exit(1)
-}
-  
+    } catch (error) {
+        console.error(error);
+        process.exit(1)
+    }
+
 }
 
 module.exports = connectDB;
--- utils/CONFIG/env.config.js
+++ utils/CONFIG/env.config.js
@@ -4,7 +4,8 @@ const CONFIG = {
     port: process.env.PORT,
     mongo_uri: process.env.MONGO_URI,
     redis_url: process.env.REDIS_URL,
-    test_mongo_uri : process.env.TEST_MONGO_URI,
+    test_mongo_uri: process.env.TEST_MONGO_URI,
+    host_url: process.env.HOST_URL,
 }
 
 module.exports = CONFIG;
