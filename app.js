ccaddb519b61e1b11a64669e8c62f144fa0ea10f
walemust <walemust28@gmail.com>
2023-07-04 16:48:42 +0100

    added async function

--- app.js
+++ app.js
@@ -5,35 +5,44 @@ const app = express();
 const authRouter = require("./routes/auth.route");
 const router = require("./routes/nigeriaData.route");
 
-const newSwag = JSON.parse(readfile(new URL('./swagger-output.json', import.meta.url)))
+async function startApp() {
+    try {
+        const newSwag = JSON.parse(await readfile(new URL('./swagger-output.json', import.meta.url)))
 
 
-app.use(require("./utils/middlewares/rateLimiter"));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(require("./utils/middlewares/rateLimiter"));
+        app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({ extended: true }))
 
-//middleware to grab post/patch requests as json files or other files
-app.use(express.json());
-
-
-app.get("/", (req, res) => {
-    res.send({
-        success: true,
-        message: " Welcome to NIGERIA Locale service"
-    })
-});
+        //middleware to grab post/patch requests as json files or other files
+        app.use(express.json());
 
 
-//app middleware to the user registration & login routes
-app.use("/api/v1/auth", authRouter);
-//route path middleware
-app.use("/api/v1/search", router);
+        app.get("/", (req, res) => {
+            res.send({
+                success: true,
+                message: " Welcome to NIGERIA Locale service"
+            })
+        });
 
-//404 handler
-app.all("*", require("./utils/error/404.error"))
-//global error handler
-app.use(require("./utils/error/globalError"))
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(newSwag, { explorer: true }));
+        //app middleware to the user registration & login routes
+        app.use("/api/v1/auth", authRouter);
+        //route path middleware
+        app.use("/api/v1/search", router);
+
+        //404 handler
+        app.all("*", require("./utils/error/404.error"))
+        //global error handler
+        app.use(require("./utils/error/globalError"))
+
+        app.use('/docs', swaggerUi.serve, swaggerUi.setup(newSwag, { explorer: true }));
+    }
+    catch (error) {
+        console.error('Error reading Swagger file:', error);
+    }
+}
+
+startApp();
 
 module.exports = app
