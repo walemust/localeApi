bde3ef7b5e3f2694bd39f196d68725a3c2925703
walemust <walemust28@gmail.com>
2023-07-06 10:29:57 +0100

    corrected the route

--- routes/nigeriaData.route.js
+++ routes/nigeriaData.route.js
@@ -1,8 +1,60 @@
-require("dotenv").config()
-const express = require("express");
+// require("dotenv").config()
+// const express = require("express");
 
+// const cacheMiddleware = require("../utils/middlewares/cache.middleware")
+// const validateApiKey = require("../utils/middlewares/authMiddleWare");
+// const {
+//     getData,
+//     getRegions,
+//     getStates,
+//     getRegionState,
+//     getStateLga,
+//     getStateMetadata,
+//     getOneRegion,
+//     getOneState,
+// } = require("../controllers/nigeriaData.controller")
+
+// const router = express.Router();
+
+// if (process.env.NODE_ENV !== 'test') {
+//     router.get("/", validateApiKey, getData);
+
+//     router.get("/regions", validateApiKey, cacheMiddleware, getRegions);
+
+//     router.get("/states", validateApiKey, cacheMiddleware, getStates);
+
+//     router.get("/region-state", validateApiKey, cacheMiddleware, getRegionState);
+
+//     router.get("/state-lga", validateApiKey, cacheMiddleware, getStateLga);
+
+//     router.get("/state-data", validateApiKey, cacheMiddleware, getStateMetadata);
+
+
+//     //----------------Query A value-----------------------
+
+//     router.get("/regions/:regionName", validateApiKey, cacheMiddleware, getOneRegion);
+
+
+//     router.get("/states/:stateName", validateApiKey, cacheMiddleware, getOneState);
+// } else {
+//     router.get("/", validateApiKey, cacheMiddleware, getData);
+//     router.get("/regions", validateApiKey, cacheMiddleware, getRegions);
+//     router.get("/states", validateApiKey, cacheMiddleware, getStates);
+//     router.get("/region-states", validateApiKey, cacheMiddleware, getRegionState);
+//     router.get("/state-lgas", validateApiKey, cacheMiddleware, getStateLga);
+//     router.get("/state-data", validateApiKey, cacheMiddleware, getStateMetadata);
+//     // //----------------Query A value-----------------------
+//     router.get("/regions/:regionName", validateApiKey, cacheMiddleware, getOneRegion);
+//     //Gets details about a state
+//     router.get("/states/:stateName", validateApiKey, cacheMiddleware, getOneState);
+// }
+
+// module.exports = router;
+
+require("dotenv").config()
+const express = require("express")
 const cacheMiddleware = require("../utils/middlewares/cache.middleware")
-const validateApiKey = require("../utils/middlewares/authMiddleWare");
+const validateApiKey = require("../utils/middlewares/authMiddleWare")
 const {
     getData,
     getRegions,
@@ -16,27 +68,19 @@ const {
 
 const router = express.Router();
 
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV === 'test') {
     router.get("/", validateApiKey, getData);
-
-    router.get("/regions", validateApiKey, cacheMiddleware, getRegions);
-
-    router.get("/states", validateApiKey, cacheMiddleware, getStates);
-
-    router.get("/region-state", validateApiKey, cacheMiddleware, getRegionState);
-
-    router.get("/state-lga", validateApiKey, cacheMiddleware, getStateLga);
-
-    router.get("/state-data", validateApiKey, cacheMiddleware, getStateMetadata);
-
-
-    //----------------Query A value-----------------------
-
-    router.get("/regions/:regionName", validateApiKey, cacheMiddleware, getOneRegion);
-
-
-    router.get("/states/:stateName", validateApiKey, cacheMiddleware, getOneState);
+    router.get("/regions", validateApiKey, getRegions);
+    router.get("/states", validateApiKey, getStates);
+    router.get("/region-states", validateApiKey, getRegionState);
+    router.get("/state-lgas", validateApiKey, getStateLga);
+    router.get("/state-data", validateApiKey, getStateMetadata);
+    // //----------------Query A value-----------------------
+    router.get("/regions/:regionName", validateApiKey, getOneRegion);
+    //Gets details about a state
+    router.get("/states/:stateName", validateApiKey, getOneState);
 } else {
+
     router.get("/", validateApiKey, cacheMiddleware, getData);
     router.get("/regions", validateApiKey, cacheMiddleware, getRegions);
     router.get("/states", validateApiKey, cacheMiddleware, getStates);
@@ -47,6 +91,20 @@ if (process.env.NODE_ENV !== 'test') {
     router.get("/regions/:regionName", validateApiKey, cacheMiddleware, getOneRegion);
     //Gets details about a state
     router.get("/states/:stateName", validateApiKey, cacheMiddleware, getOneState);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+// Gets details about a region.
+
 
 module.exports = router;
--- utils/authMiddleWare.js
+++ utils/authMiddleWare.js
@@ -24,30 +24,3 @@ const validateApiKey = (async (req, res, next) =>{
 
 
 module.exports = validateApiKey;
-
-
-
-// const authenticate = (req, res, next) => {
-//     const authHeader = req.headers.authorization;
-//     if(!authHeader  ||  ! authHeader.startsWith('Bearer ')){
-//         throw new Error('Invalid Authentication')
-//     }
-    
-
-//     const token = authHeader.split(" ")[1];
-//     // console.log(token)
-
-//     try {
-//         const payload = jwt.verify(token, CONFIG.jwt_secret)
-
-//         //useful when attaching the user[author] to the protected articles route
-//         // req.user = {userID : payload.userID, email : payload.email, username : payload.username}
-//         next()
-
-//     } catch (error) {
-//         throw new Error('Invalid Authentication')
-
-//     }
-// }
-
-// module.exports = authenticate
